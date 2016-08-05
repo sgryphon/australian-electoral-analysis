745b5aa55ede867c57f8d9e9a5516bebd2732736
Sly Gryphon <sgryphon@computer.org>
2016-08-06 08:17:18 +1000

    Add skip support to vote import, but then realised it was the max SQLEXPRESS limit (so have to just use separate DB per state in express)

--- Scripts/Import-AESenateVotes.ps1
+++ Scripts/Import-AESenateVotes.ps1
@@ -11,7 +11,8 @@ param
 	[string] $State = "NT",
 	[string] $FilePath = "..\SampleData\aec-senate-formalpreferences-20499-NT.csv",
 	[string] $ServerInstance = ".\SQLEXPRESS",
-	[string] $Database = "ElectoralAnalysis"
+	[string] $Database = "ElectoralAnalysis",
+	[int] $Skip = 0
 )
 Set-StrictMode -Version Latest
 $scriptRoot = $MyInvocation.MyCommand.Path | Split-Path
@@ -41,14 +42,26 @@ Write-Host $query
 Invoke-SqlCmd -ServerInstance $ServerInstance -Database $Database -Query $query
 #>
 
-$data = Import-Csv $dataFile
-
-$total = $data.Count
-Write-Verbose "Importing $total votes from data file"
-$count = 0
-$skipped = 0
 $escapedElection = $Election -replace "'", "''"
 $escapedState = $State -replace "'", "''"
+
+$existing = Invoke-SqlCmd -ServerInstance $ServerInstance -Database $Database -Query "SELECT COUNT(*) AS CountRecords FROM [RawSenateFormalPreferences] WHERE Election = '$escapedElection' AND StateAb = '$escapedState';"
+$startRecords = $existing.CountRecords 
+Write-Verbose "Existing database has $($startRecords) records for election/state"
+
+$data = Import-Csv $dataFile
+$total = $data.Count
+Write-Verbose "Importing $total votes from data file"
+
+$skipFlag = $false
+if ($Skip) {
+	Write-Verbose "Skipping $Skip records"
+	$skipFlag = $true
+}
+
+$count = 0
+$skippedHeader = 0
+$skippedRecords = 0
 $batchDataSelect = ""
 $batchSize = 1000
 Write-Verbose "Batch size: $batchSize"
@@ -56,8 +69,18 @@ Write-Progress -Activity "Importing $State" -PercentComplete ($count * 100 / $to
 foreach ($row in $data) {
     $count++;
 	if ($count -le 1) { 
-		$skipped++;
+		$skippedHeader++;
 		continue; 
+	}
+	
+	if ($count -le ($Skip + 1)) {
+		$skippedRecords++;
+		continue;
+	}
+	
+	if ($skipFlag) {
+		Write-Verbose "Skipped $skippedRecords records, starting processing"
+		$skipFlag = $false
 	}
 	
 	if ($batchDataSelect) { 
@@ -109,7 +132,9 @@ if ($batchDataSelect) {
 }
 
 $result = Invoke-SqlCmd -ServerInstance $ServerInstance -Database $Database -Query "SELECT COUNT(*) AS CountRecords FROM [RawSenateFormalPreferences] WHERE Election = '$escapedElection' AND StateAb = '$escapedState';"
-Write-Verbose "Result imported $($result.CountRecords) records, skipped $skipped"
+Write-Verbose "Result total $($result.CountRecords) records"
+Write-Verbsoe "Skipped header lines: $skippedHeader"
+Write-Verbsoe "Skipped records: $skippedRecords"
 	
 $endTime = [datetimeoffset]::Now
 Write-Verbose "Import $State finish $($endTime.ToString("yyyy-MM-dd HH:mm:ss"))"
